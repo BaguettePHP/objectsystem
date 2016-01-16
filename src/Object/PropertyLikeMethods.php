95cf6488dd7c3c29bdc6ba36242d2533c2af1fa4
USAMI Kenta <tadsan@zonu.me>
2016-01-16 17:20:19 +0900

    Add PropertyLikeMethods trait

---
+++ src/Object/PropertyLikeMethods.php
@@ -0,0 +1,40 @@
+<?php
+namespace Teto\Object;
+
+/**
+ * Make Property like method
+ *
+ * @package    Teto
+ * @subpackage Object
+ * @copyright  2014 USAMI Kenta
+ * @license    http://www.apache.org/licenses/LICENSE-2.0
+ * @author     USAMI Kenta <tadsan@zonu.me>
+ */
+trait PropertyLikeMethods
+{
+    /**
+     * Call method as property (magic method)
+     *
+     * @param  string $name
+     * @return mixed
+     * @link   http://php.net/manual/language.oop5.magic.php
+     */
+    public function __get($name)
+    {
+        if (!isset(self::$property_like_methods)) {
+            throw new \LogicException(static::class.'::$property_like_methods is not set.');
+        }
+
+        if (isset(self::$property_like_methods[$name])) {
+            $method = self::$property_like_methods[$name];
+        } elseif (in_array($name, self::$property_like_methods)) {
+            $method = $name;
+        } elseif (property_exists($this, $name)) {
+            return $this->$name;
+        } else {
+            throw new \OutOfRangeException("Unexpected key:'$name'");
+        }
+
+        return call_user_func([$this, $method]);
+    }
+}
---
+++ tests/Object/PropertyLikeMethodsTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace Teto\Object;
+
+/**
+ * @package    Teto
+ * @subpackage Object
+ * @copyright  2015 USAMI Kenta
+ * @license    http://www.apache.org/licenses/LICENSE-2.0
+ * @author     USAMI Kenta <tadsan@zonu.me>
+ */
+class PropertyLikeMethodsTestModel
+{
+    use PropertyLikeMethods;
+
+    private $string;
+
+    private static $property_like_methods = [
+        'mb_strlen',
+        'len' => 'strlen',
+    ];
+
+    public function __construct($string)
+    {
+        $this->string = $string;
+    }
+
+    public function strlen()
+    {
+        return strlen($this->string);
+    }
+
+    public function mb_strlen()
+    {
+        return mb_strlen($this->string, 'UTF-8');
+    }
+}
+
+/**
+ * @package    Teto
+ * @subpackage Object
+ * @copyright  2015 USAMI Kenta
+ * @license    http://www.apache.org/licenses/LICENSE-2.0
+ * @author     USAMI Kenta <tadsan@zonu.me>
+ */
+class PropertyLikeMethodsTest extends \Teto\TestCase
+{
+    public function test()
+    {
+        $val = new PropertyLikeMethodsTestModel('ぬるぽ');
+
+        $this->assertEquals($val->strlen(), $val->len);
+        $this->assertEquals($val->strlen(), $val->strlen);
+        $this->assertEquals($val->mb_strlen(), $val->mb_strlen);
+    }
+
+    public function test_raiseError()
+    {
+        $val = new PropertyLikeMethodsTestModel('ぬるぽ');
+        $this->setExpectedException(\OutOfRangeException::class, "Unexpected key:'unknown'");
+
+        $_ = $val->unknown;
+    }
+}
