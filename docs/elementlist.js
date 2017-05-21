a540a138cdb2537075b73baa0a1b609be4f44c1b
USAMI Kenta <tadsan@zonu.me>
2017-05-22 00:29:38 +0900

    Add docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Teto\\Object\\Helper"],["c","Teto\\Object\\MethodAlias"],["c","Teto\\Object\\ObjectArray"],["c","Teto\\Object\\PrivateGetter"],["c","Teto\\Object\\PrivateStrictGetter"],["c","Teto\\Object\\PropertyLikeMethod"],["c","Teto\\Object\\ReadOnly"],["c","Teto\\Object\\ToArrayInterface"],["c","Teto\\Object\\TypeAssert"],["c","Teto\\Object\\TypeDefinition"],["c","Teto\\Object\\TypedProperty"]];
